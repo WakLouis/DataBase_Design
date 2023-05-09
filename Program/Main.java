62230a0d7ffa7d8dd14e123b756ab6dae926ae26
WakLouis <1139128923@qq.com>
2023-05-09 22:00:31 +0800

    v0.1

--- Program/Main.java
+++ Program/Main.java
@@ -1,10 +1,11 @@
+
 /*
  * @Descripttion: 程序主体文件
  * @version: 
  * @Author: WakLouis
  * @Date: 2022-05-23 09:52:48
  * @LastEditors: WakLouis
- * @LastEditTime: 2022-06-05 21:24:26
+ * @LastEditTime: 2023-05-09 17:22:05
  */
 import java.awt.*;
 import javax.swing.*;
@@ -15,155 +16,169 @@ import java.awt.event.ActionEvent;
 import java.util.Date;
 
 //创建欢迎词
-class WelcomeText extends JLabel{
+class WelcomeText extends JLabel {
     Date date = new Date();
-    void welcomeText(){
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd"); 
+
+    void welcomeText() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         setText("你好! " + Panel.user.getText() + ", " + "今天是" + formatter.format(date));
-        //setBorder(BorderFactory.createLineBorder(Color.RED,1));
-        setFont(new Font("微软雅黑",Font.BOLD,18));
-        setBounds(5,5,500,20);
+        // setBorder(BorderFactory.createLineBorder(Color.RED,1));
+        setFont(new Font("微软雅黑", Font.BOLD, 18));
+        setBounds(5, 5, 500, 20);
     }
 }
 
-//窗口界面
-class Panel extends JPanel{
-    //提交按钮
+// 窗口界面
+class Panel extends JPanel {
+    // 提交按钮
     static JButton submitButton;
-    //输入框
+    // 输入框
     static JTextField user, paswd;
-    //显示面板
+    // 显示面板
     static JTextArea displayArea;
     static JTextArea displayAreaForCtrl;
+    // 显示面板的滚动条
+    static JScrollPane displayScorllPane;
+    static JScrollPane displayScorllPaneForCtrl;
 
-    void loginPanel(){
+    void loginPanel() {
         setLayout(null);
         setVisible(true);
-        
-        //输入框
+
+        // 输入框
         user = new JTextField();
         paswd = new JTextField();
         user.addFocusListener(new JTextFieldHintListener(user, "请输入用户名"));
         paswd.addFocusListener(new JTextFieldHintListener(paswd, "请输入密码"));
-        user.setBounds(300,170,200,40);
-        paswd.setBounds(300,220,200,40);
-        add(user); add(paswd);
+        user.setBounds(300, 170, 200, 40);
+        paswd.setBounds(300, 220, 200, 40);
+        add(user);
+        add(paswd);
 
-        //主体文字
+        // 主体文字
         JLabel loginText = new JLabel("用户登录");
-        loginText.setFont(new Font("宋体",Font.BOLD,30));
-        loginText.setBounds(340,50,200,100);
+        loginText.setFont(new Font("宋体", Font.BOLD, 30));
+        loginText.setBounds(340, 50, 200, 100);
         add(loginText);
 
-        //提示文字
+        // 提示文字
         JLabel tipText = new JLabel("");
         tipText.setBounds(350, 300, 200, 40);
         add(tipText);
 
-        //提交按钮功能实现
+        // 提交按钮功能实现
         submitButton = new JButton("登录");
-        submitButton.setBounds(300,270,200,40);
-        submitButton.addActionListener(new ActionListener(){
+        submitButton.setBounds(300, 270, 200, 40);
+        submitButton.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 // TODO Auto-generated method stub
                 boolean flag = Program.connection(user.getText(), paswd.getText());
-                if(flag == true){
+                if (flag == true) {
                     Main.loginFrame.dispose();
                     Main.mainFrame = new JFrame();
                     Main.mainFrameInit(Main.mainFrame);
-                }
-                else{
+                } else {
                     tipText.setText("用户名或密码错误");
                 }
-                //Main.loginFrame.dispose();
+                // Main.loginFrame.dispose();
             }
-            
+
         });
         add(submitButton);
     }
 
-    void checkPanel(){
+    void checkPanel() {
         setVisible(true);
         setLayout(null);
-        
+
         WelcomeText welcomeText = new WelcomeText();
         welcomeText.welcomeText();
         add(welcomeText);
 
         JButton checkButton = new JButton("一键打卡");
-        checkButton.setBounds(0,30,201,70);
+        checkButton.setBounds(0, 30, 201, 70);
         checkButton.setContentAreaFilled(false);
         checkButton.setFocusPainted(false);
         add(checkButton);
 
         JButton queryButton = new JButton("考勤查询");
-        queryButton.setBounds(0,99,201,70);
+        queryButton.setBounds(0, 99, 201, 70);
         queryButton.setContentAreaFilled(false);
         queryButton.setFocusPainted(false);
         add(queryButton);
 
         displayArea = new JTextArea();
-        displayArea.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
-        displayArea.setBounds(200,30,590,470);
+        displayArea.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
         displayArea.setEditable(false);
-        add(displayArea);
 
-        checkButton.addActionListener(new ActionListener(){
+        displayScorllPane = new JScrollPane();
+        displayScorllPane.setBounds(200, 30, 590, 400);
+        displayScorllPane.setViewportView(displayArea);
+
+        add(displayScorllPane);
+
+        checkButton.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 // TODO Auto-generated method stub
-                if(Program.fCheck()){
+                if (Program.fCheck()) {
                     displayArea.append("每日打卡成功！\n");
-                    Panel.displayArea.append("\n_________________________________________________________________________________\n");
-                }
-                else{
+                    Panel.displayArea.append(
+                            "\n_________________________________________________________________________________\n");
+                } else {
                     displayArea.append("每日打卡失败！\n");
-                    Panel.displayArea.append("\n_________________________________________________________________________________\n");
+                    Panel.displayArea.append(
+                            "\n_________________________________________________________________________________\n");
                 }
-                //displayArea.append("每日打卡成功！\n");
+                // displayArea.append("每日打卡成功！\n");
             }
         });
-        queryButton.addActionListener(new ActionListener(){
+        queryButton.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 // TODO Auto-generated method stub
                 Program.fQuery();
-                
+
             }
         });
     }
 
-    void ctrlPanel(){
+    void ctrlPanel() {
         setVisible(true);
         setLayout(null);
 
         WelcomeText welcomeText = new WelcomeText();
         welcomeText.welcomeText();
         add(welcomeText);
-        
+
         JButton checkButtonForCtrl = new JButton("查看考勤表");
-        checkButtonForCtrl.setBounds(0,30,201,70);
+        checkButtonForCtrl.setBounds(0, 30, 201, 70);
         checkButtonForCtrl.setContentAreaFilled(false);
         checkButtonForCtrl.setFocusPainted(false);
         add(checkButtonForCtrl);
 
         JButton updateButton = new JButton("修改考勤信息");
-        updateButton.setBounds(0,99,201,70);
+        updateButton.setBounds(0, 99, 201, 70);
         updateButton.setContentAreaFilled(false);
         updateButton.setFocusPainted(false);
         add(updateButton);
 
         displayAreaForCtrl = new JTextArea();
-        displayAreaForCtrl.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
-        displayAreaForCtrl.setBounds(200,30,590,470);
+        displayAreaForCtrl.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        displayAreaForCtrl.setBounds(200, 30, 590, 470);
         displayAreaForCtrl.setEditable(false);
-        add(displayAreaForCtrl);
 
-        JTextField upName,upDate,upTo;
+        displayScorllPaneForCtrl = new JScrollPane();
+        displayScorllPaneForCtrl.setBounds(200, 30, 590, 400);
+        displayScorllPaneForCtrl.setViewportView(displayAreaForCtrl);
+
+        add(displayScorllPaneForCtrl);
+
+        JTextField upName, upDate, upTo;
         upName = new JTextField();
         upName.addFocusListener(new JTextFieldHintListener(upName, "需更新用户名"));
         upDate = new JTextField();
@@ -171,15 +186,15 @@ class Panel extends JPanel{
         upTo = new JTextField();
         upTo.addFocusListener(new JTextFieldHintListener(upTo, "修改值为"));
 
-        upName.setBounds(0,169,201,30);
-        upDate.setBounds(0,199,201,30);
-        upTo.setBounds(0,229,201,30);
+        upName.setBounds(0, 169, 201, 30);
+        upDate.setBounds(0, 199, 201, 30);
+        upTo.setBounds(0, 229, 201, 30);
 
         add(upName);
         add(upDate);
         add(upTo);
 
-        checkButtonForCtrl.addActionListener(new ActionListener(){
+        checkButtonForCtrl.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -188,7 +203,7 @@ class Panel extends JPanel{
             }
         });
 
-        updateButton.addActionListener(new ActionListener(){
+        updateButton.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -200,8 +215,7 @@ class Panel extends JPanel{
     }
 }
 
-
-public class Main{
+public class Main {
 
     static CardLayout cardLayout = new CardLayout();
     static Panel panel = new Panel();
@@ -215,12 +229,12 @@ public class Main{
     static JMenuItem ctrlOption = new JMenuItem("修改信息");
     static JMenuItem checkOption = new JMenuItem("打卡查询");
 
-    static void loginFrameInit(JFrame f){
-        //f.setLayout(null);
-        f.setLocation(400,200);
+    static void loginFrameInit(JFrame f) {
+        // f.setLayout(null);
+        f.setLocation(400, 200);
         f.setTitle("登录");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(790,500);
+        f.setSize(790, 500);
         f.setVisible(true);
 
         panel.loginPanel();
@@ -229,36 +243,36 @@ public class Main{
 
     }
 
-    static void mainFrameInit(JFrame f){
+    static void mainFrameInit(JFrame f) {
         f.setTitle("数据库管理页面");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(790,500);
+        f.setSize(790, 500);
         f.setVisible(true);
-        f.setLocation(400,200);
+        f.setLocation(400, 200);
 
-        //创建Frame面板
+        // 创建Frame面板
         contentPane = new JPanel();
-        //contentPane.setLayout(null);
+        // contentPane.setLayout(null);
         contentPane.setLayout(cardLayout);
         f.setContentPane(contentPane);
 
-        //创建check面板
+        // 创建check面板
         Panel checkPanel = new Panel();
         checkPanel.checkPanel();
-        contentPane.add(checkPanel,"check");
+        contentPane.add(checkPanel, "check");
 
-        //创建ctrl面板
+        // 创建ctrl面板
         Panel ctrlPanel = new Panel();
         ctrlPanel.ctrlPanel();
-        contentPane.add(ctrlPanel,"ctrl");
+        contentPane.add(ctrlPanel, "ctrl");
 
-        //创建菜单栏
+        // 创建菜单栏
         checkMenu.add(checkOption);
         ctrlMenu.add(ctrlOption);
         menuBar.add(checkMenu);
         menuBar.add(ctrlMenu);
         f.setJMenuBar(menuBar);
-        checkOption.addActionListener(new ActionListener(){
+        checkOption.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -266,7 +280,7 @@ public class Main{
                 cardLayout.show(contentPane, "check");
             }
         });
-        ctrlOption.addActionListener(new ActionListener(){
+        ctrlOption.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
--- Program/Program.java
+++ Program/Program.java
@@ -4,7 +4,7 @@
  * @Author: WakLouis
  * @Date: 2022-05-23 09:31:43
  * @LastEditors: WakLouis
- * @LastEditTime: 2022-06-05 21:43:37
+ * @LastEditTime: 2023-05-09 21:32:28
  */
 
 import java.sql.Connection;
@@ -12,13 +12,18 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-
+import java.sql.Time;
+import java.time.LocalTime;
 
 public class Program {
     static String connectionUrl;
+
+    public static java.sql.Time toSqlTime(LocalTime localTime) {
+        return java.sql.Time.valueOf(localTime);
+    }
+
     public static boolean connection(String user, String paswd) {
-        connectionUrl =
-            "jdbc:sqlserver://127.0.0.1:1433;"
+        connectionUrl = "jdbc:sqlserver://192.168.137.128:1433;"
                 + "database=DataBaseDesign;"
                 + "user=" + user + ";"
                 + "password=" + paswd + ";"
@@ -28,8 +33,8 @@ public class Program {
 
         try (Connection conn = DriverManager.getConnection(connectionUrl);) {
             conn.close();
-        	return true;
-            
+            return true;
+
         }
         // Handle any errors that may have occurred.
         catch (SQLException e) {
@@ -37,26 +42,42 @@ public class Program {
             return false;
         }
     }
-    
-    public static boolean fCheck(){
-        try{
+
+    public static boolean fCheck() {
+        try {
             Connection con = DriverManager.getConnection(connectionUrl);
-            String sql = "EXEC proc_check";
+            String sql;
             java.sql.Statement st = con.createStatement();
+            int flag = 1;
+
+            // 检测今天是否打卡
+
+            // 打卡前检测是否已经迟到
+            sql = "SELECT 上班时间 from dbo.管理信息";
+            ResultSet rs = st.executeQuery(sql);
+            rs.next();
+            Time officeTime = rs.getTime("上班时间");
+            if (officeTime.before(toSqlTime(LocalTime.now()))) {
+                Panel.displayArea.append(String.format("上班时间为：" + officeTime + "    您已迟到！\n\n"));
+                flag = 2;
+            }
+
+            // 打卡
+            sql = ("EXEC proc_check @checkFlag = " + flag).toString();
+
             st.execute(sql);
 
             st.close();
             con.close();
             return true;
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    public static void fQuery(){
-        try{
+    public static void fQuery() {
+        try {
             Connection con = DriverManager.getConnection(connectionUrl);
             String sql = "select * from 数据总表 where 员工编号 = SUSER_NAME()";
             java.sql.Statement st = con.createStatement();
@@ -64,63 +85,67 @@ public class Program {
             ResultSetMetaData md = rs.getMetaData();
             int num = md.getColumnCount();
             rs.next();
-            for(int i = 1;i <= md.getColumnCount();i++){
+            for (int i = 1; i <= md.getColumnCount(); i++) {
                 String columnName = md.getColumnName(i);
-                Panel.displayArea.append(String.format("%s\t",columnName));
+                Panel.displayArea.append(String.format("%s\t", columnName));
             }
             Panel.displayArea.append("\n");
-            for(int i = 1;i <= md.getColumnCount();i++){
-                Panel.displayArea.append(String.format("%s\t",rs.getObject(i)));
+            for (int i = 1; i <= md.getColumnCount(); i++) {
+                Panel.displayArea.append(String.format("%s\t", rs.getObject(i)));
             }
-            Panel.displayArea.append(String.format("\n您的考勤率为：%.1f", Float.parseFloat(rs.getString("考勤天数")) / (num-2) ) );
-            Panel.displayArea.append("\n_________________________________________________________________________________\n");
+            Panel.displayArea
+                    .append(String.format("\n您的考勤率为：%.1f", Float.parseFloat(rs.getString("考勤天数")) / (num - 2)));
+            Panel.displayArea
+                    .append("\n_________________________________________________________________________________\n");
             rs.close();
             con.close();
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             Panel.displayArea.append("操作失败！\n");
-            Panel.displayArea.append("\n_________________________________________________________________________________\n");
+            Panel.displayArea
+                    .append("\n_________________________________________________________________________________\n");
             e.printStackTrace();
         }
 
     }
 
-    public static void fQueryAll(){
-        if(Panel.user.getText().equals("1001")){
+    public static void fQueryAll() {
+        if (Panel.user.getText().equals("1001")) {
             Panel.displayAreaForCtrl.append("操作失败！\n");
-            Panel.displayAreaForCtrl.append("\n_________________________________________________________________________________\n");
+            Panel.displayAreaForCtrl
+                    .append("\n_________________________________________________________________________________\n");
             return;
         }
-        try{
+        try {
             Connection con = DriverManager.getConnection(connectionUrl);
             String sql = "select * from 数据总表";
             java.sql.Statement st = con.createStatement();
             ResultSet rs = st.executeQuery(sql);
             ResultSetMetaData md = rs.getMetaData();
-            for(int i = 1;i <= md.getColumnCount();i++){
+            for (int i = 1; i <= md.getColumnCount(); i++) {
                 String columnName = md.getColumnName(i);
-                Panel.displayAreaForCtrl.append(String.format("%s\t",columnName));
+                Panel.displayAreaForCtrl.append(String.format("%s\t", columnName));
             }
-            while(rs.next()){
+            while (rs.next()) {
                 Panel.displayAreaForCtrl.append("\n");
-                for(int i = 1;i <= md.getColumnCount();i++){
-                    Panel.displayAreaForCtrl.append(String.format("%s\t",rs.getObject(i)));
+                for (int i = 1; i <= md.getColumnCount(); i++) {
+                    Panel.displayAreaForCtrl.append(String.format("%s\t", rs.getObject(i)));
                 }
             }
-            Panel.displayAreaForCtrl.append("\n_________________________________________________________________________________\n");
+            Panel.displayAreaForCtrl
+                    .append("\n_________________________________________________________________________________\n");
             rs.close();
             con.close();
 
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void fUpdate(String Name,String Date,String To){
-        if(Panel.user.getText().equals("1001")){
+    public static void fUpdate(String Name, String Date, String To) {
+        if (Panel.user.getText().equals("1001")) {
             Panel.displayAreaForCtrl.append("操作失败！\n");
-            Panel.displayAreaForCtrl.append("\n_________________________________________________________________________________\n");
+            Panel.displayAreaForCtrl
+                    .append("\n_________________________________________________________________________________\n");
             return;
         }
         try {
@@ -128,11 +153,13 @@ public class Program {
             String sql = "update 数据总表 set \"" + Date + "\" = " + To + " where 员工编号 = " + Name;
             java.sql.Statement st = con.createStatement();
             st.execute(sql);
-            Panel.displayAreaForCtrl.append("操作成功!已将"+Name+"在"+Date+"的值设置为："+To+"\n");
-            Panel.displayAreaForCtrl.append("\n_________________________________________________________________________________\n");
+            Panel.displayAreaForCtrl.append("操作成功!已将" + Name + "在" + Date + "的值设置为：" + To + "\n");
+            Panel.displayAreaForCtrl
+                    .append("\n_________________________________________________________________________________\n");
         } catch (SQLException e) {
             Panel.displayAreaForCtrl.append("操作失败！\n");
-            Panel.displayAreaForCtrl.append("\n_________________________________________________________________________________\n");
+            Panel.displayAreaForCtrl
+                    .append("\n_________________________________________________________________________________\n");
             e.printStackTrace();
         }
     }
